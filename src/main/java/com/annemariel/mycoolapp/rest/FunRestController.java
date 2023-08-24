e430e67d64407bcb107436ac8e8ab2249d220437
AnneMariel95 <a.mariel.recio@gmail.com>
2023-08-24 09:05:32 +0200

    initial commit

---
+++ src/main/java/com/annemariel/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,13 @@
+package com.annemariel.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
